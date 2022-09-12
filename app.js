7e6279f836b6d567b20f2d68cc7bf908e0852e6c
François Devantay <francois.devantay@studentfr.ch>
2022-09-12 16:14:58 +0200

    done index, start homepage

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ document.body.addEventListener("mousemove", function(event) {
 
 var svgCursor = document.getElementById('svg-cursor');
 function moveCursor(e) {
-  var x = e.clientX - 15,
-      y = e.clientY - 15;
-  svgCursor.setAttribute('style', 'left: ' + x + 'px; top: ' + y + 'px');
+    var x = e.clientX - 0,
+        y = e.clientY - 0;
+    svgCursor.setAttribute('style', 'left: ' + x + 'px; top: ' + y + 'px');
+    if (e.target.className.indexOf('custom-cursor') > -1) {
+        switch (e.target.className) {
+            case 'custom-cursor custom-cursor--action':
+                svgCursor.setAttribute('class', 'svg-cursor svg-cursor__action')
+                break;
+            case 'custom-cursor custom-cursor--close':
+                svgCursor.setAttribute('class', 'svg-cursor svg-cursor__close')
+                break;
+        }
+    } else {
+        svgCursor.setAttribute('class', 'svg-cursor');
+    }
 }
-
 console.log(moveCursor)
-console.log(x)
