d18d42691e55d42540ca645958fa3323c7352e20
François Devantay <francois.devantay@studentfr.ch>
2022-09-13 22:36:46 +0200

    all the issues are fixed

--- app.js
+++ app.js
@@ -1,39 +1,9 @@
-document.addEventListener("mousemove", function (event) {
-  moveCursor(event);
-});
+var cursor = document.querySelector('.cursor')
 
-var svgCursor = document.getElementById("svg-cursor");
-function moveCursor(e) {
-  var x = e.clientX - 0,
-      y = e.clientY - 0;
-  svgCursor.setAttribute("style", "left: " + x + "px; top: " + y + "px");
-  if (e.target.className.indexOf("custom-cursor") > -1) {
-    switch (e.target.className) {
-      case "custom-cursor custom-cursor--action":
-        svgCursor.setAttribute("class", "svg-cursor svg-cursor__action");
-        break;
-      case "custom-cursor custom-cursor--close":
-        svgCursor.setAttribute("class", "svg-cursor svg-cursor__close");
-        break;
-    }
-  } else {
-    svgCursor.setAttribute("class", "svg-cursor");
-  }
-}
-console.log(moveCursor);
-
-document.addEventListener("hovering", function (event) {
-  onmouseover(event);
-});
-
-var button = document.querySelector('#button-index');
-
-function hovering(event) {
-  if (button = "hovering") {
-    document.querySelector('.button-bg').classList.add('hovering-button')
-  } else {
-    document.querySelector('.button-bg').classList.remove('hovering-button')
-  }
-}
-
-console.log(hovering)
+window.addEventListener('mousemove', function(e) {
+  const x = e.clientX
+  const y = e.clientY
+  
+  cursor.style.left = x - 0 + 'px'
+  cursor.style.top = y - 0 + 'px'
+})
